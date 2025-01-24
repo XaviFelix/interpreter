4352d00c1fa4b88bc4dd172b4bd451c0d1fb8687
xfelix <xavierfelix74@gmail.com>
2025-01-23 19:41:25 -0800

    identifier support

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -36,6 +36,8 @@ func (l *Lexer) readChar() {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -56,12 +58,45 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
 	return tok
 }
 
+// all white spaces are ignored
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+// if the token is an identifer then this function
+// will read each char from the input and extract the
+// identifer and create a token out of it
+func (l *Lexer) readIdentifier() string {
+
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position] // this is a slice: input[start:end] start is inclusive, end is exclusive
+}
+
+// Checks to see if the current byte is a letter represented in the ascii table
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+// creates a new token "instance"
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
